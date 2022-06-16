dfaa11feede3255edfadcd257f7f3798898d98bb
Est200 <olajideesther2019@gmail.com>
2022-06-16 01:23:39 +0100

    init

---
+++ EstherOlajide/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class Post(models.Model):
+    Title=models.CharField(max_length=200)
+    Text=models.TextField()
+    Author=models.ForeignKey(get_user_model(),null=True, on_delete=models.CASCADE)
+    Created_date=models.DateTimeField(auto_now= True)
+    Published_date=models.DateTimeField(auto_now= True)
+    def __str__(self) -> str:
+        return self.Title
